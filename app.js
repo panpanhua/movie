51c2c30c1a2f572f9173019cc923936b49203d51
panpanhua <crackman@qq.com>
2016-09-02 09:33:00 +0800

    模板布局

--- app.js
+++ app.js
@@ -7,3 +7,28 @@ app.set('view engine','jade')
 app.listen(port)
 
 console.log('imooc started on port' + port)
+
+// index page
+app.get('/',function(req,res){
+	res.render('index', {
+		title:'微信看电影'
+	})
+})
+//list page
+app.get('/admin/list',function(req,res){
+	res.render('list', {
+		title:'微信看电影'
+	})
+})
+//detail page
+app.get('/movie/:id',function(req,res){
+	res.render('detail', {
+		title:'微信看电影'
+	})
+})
+//detail page
+app.get('/admin/movie',function(req,res){
+	res.render('admin', {
+		title:'微信看电影'
+	})
+})
